c3f8d01df6db0beb70a1fef21f4311812411d233
姚康明 <154518484@qq.com>
2018-07-20 16:28:11 +0800

    init.

---
+++ service-producer-api/src/main/java/com/github/service/UserServiceFeign.java
@@ -0,0 +1,21 @@
+package com.github.service;
+
+import com.github.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *  service-producer
+ */
+@RequestMapping("user")
+public interface UserServiceFeign {
+
+	@RequestMapping("hello")
+	String hello();
+
+	@GetMapping("{id}")
+	User getUser(@PathVariable("id") Integer id);
+
+}
+
---
+++ service-producer-core/src/main/java/com/github/service/UserService.java
@@ -0,0 +1,23 @@
+package com.github.service;
+
+import com.github.model.User;
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class UserService {
+
+	public User getUser(Integer id) {
+		return new User(id, id + "成都iMiracle123456", 28);
+	}
+
+	public String hello() {
+		return "hello: " + DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+	}
+
+	public User userInfo(Integer id) {
+		return new User(id, id + "成都iMiracle123456", 28);
+	}
+}
